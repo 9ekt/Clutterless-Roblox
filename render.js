ed33630545a0349ea3c829b9a624152e761a10ae
Preston <129973190+wutpreston@users.noreply.github.com>
2025-03-16 17:53:07 +0000

    1 new console message
    Added additional console message

--- render.js
+++ render.js
@@ -3,7 +3,7 @@ setTimeout(() => {
     console.log("%c▶ Making navigation on roblox great again!", "font-size:0.7rem;font-style:italic;color:lightgreen;")
 
     // support us message
-    console.log("%c▶ Want to support this project? Feel free to at our github, we're open source! [github link]", "font-size:0.7rem;font-style:italic;color:orange;")
+    console.log("%c▶ Feel like Roblox is still too cluttered and want to contribute to this project? Feel free to at our github, we're open source! https://github.com/wutpreston/Clutterless-Roblox", "font-size:0.7rem;font-style:italic;color:orange;")
 }, 1000)
 
 function waitForElm(selector) { // From https://stackoverflow.com/a/61511955 (Minor change to select All elements with id)
